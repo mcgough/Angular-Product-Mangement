aafddaae53f8eadb2433ad98be60b66e9f057f44
dan_mcgough <daniel.e.mcgough@avanade.com>
2017-10-09 16:25:28 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  public pageTitle: string = 'Product Management';
+}
