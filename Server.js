c3df6effa8d47aca0d57ade729fea63498fab42f
dan_mcgough <daniel.e.mcgough@avanade.com>
2017-10-17 12:15:55 -0700

    server config

--- Server.js
+++ Server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const forceSSL = () => {
+  return (req,res,next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  };
+};
+
+app.use(forceSSL());
 
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', (req,res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.listen(process.env.Port || 8080);
