d81a790a3c1491cf0eebc3ee7311d259daad5a74
dan_mcgough <daniel.e.mcgough@avanade.com>
2017-10-17 12:39:34 -0700

    404 redirected to home

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from '../home/welcome.component';
-import { PageNotFoundComponent } from '../home/page-not-found/page-not-found.component';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: '', component: WelcomeComponent },
-      { path: '**', component: PageNotFoundComponent },      
+      { path: '**', component: WelcomeComponent }, //TODO: ADD 404      
     ]),
   ],
   exports: [ RouterModule ],
